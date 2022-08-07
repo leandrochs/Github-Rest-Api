15e61b581cf76e6e332034704553072bbd20e679
leandrochs <leandrochagask@gmail.com>
2022-08-07 18:33:57 -0300

    feat: adiciona o logo do github

---
+++ src/components/githubLogo/GithubLogo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './githubLogo.css';
+
+export default function GithubLogo() {
+  return (
+    <img
+      className='githubLogo'
+      alt='logo Github'
+      src='https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg'
+    />
+  );
+}
