c5126ef6f0231846751135ec8a54ce747118f0d0
leandrochs <leandrochagask@gmail.com>
2022-08-07 18:39:43 -0300

    feat: adiciona mock para resposta de repositorios starred

---
+++ src/tests/Mocks/starredResponse.js
@@ -0,0 +1,148 @@
+export const starredResponse = [
+  {
+    id: 489439711,
+    node_id: 'R_kgDOHSxB3w',
+    name: 'awesome-compose',
+    full_name: 'Bel-Albuquerque/awesome-compose',
+    private: false,
+    owner: {
+      login: 'Bel-Albuquerque',
+      id: 82113544,
+      node_id: 'MDQ6VXNlcjgyMTEzNTQ0',
+      avatar_url: 'https://avatars.githubusercontent.com/u/82113544?v=4',
+      gravatar_id: '',
+      url: 'https://api.github.com/users/Bel-Albuquerque',
+      html_url: 'https://github.com/Bel-Albuquerque',
+      followers_url: 'https://api.github.com/users/Bel-Albuquerque/followers',
+      following_url:
+        'https://api.github.com/users/Bel-Albuquerque/following{/other_user}',
+      gists_url: 'https://api.github.com/users/Bel-Albuquerque/gists{/gist_id}',
+      starred_url:
+        'https://api.github.com/users/Bel-Albuquerque/starred{/owner}{/repo}',
+      subscriptions_url:
+        'https://api.github.com/users/Bel-Albuquerque/subscriptions',
+      organizations_url: 'https://api.github.com/users/Bel-Albuquerque/orgs',
+      repos_url: 'https://api.github.com/users/Bel-Albuquerque/repos',
+      events_url:
+        'https://api.github.com/users/Bel-Albuquerque/events{/privacy}',
+      received_events_url:
+        'https://api.github.com/users/Bel-Albuquerque/received_events',
+      type: 'User',
+      site_admin: false,
+    },
+    html_url: 'https://github.com/Bel-Albuquerque/awesome-compose',
+    description: 'Awesome Docker Compose samples',
+    fork: true,
+    url: 'https://api.github.com/repos/Bel-Albuquerque/awesome-compose',
+    forks_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/forks',
+    keys_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/keys{/key_id}',
+    collaborators_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/collaborators{/collaborator}',
+    teams_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/teams',
+    hooks_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/hooks',
+    issue_events_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/issues/events{/number}',
+    events_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/events',
+    assignees_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/assignees{/user}',
+    branches_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/branches{/branch}',
+    tags_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/tags',
+    blobs_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/git/blobs{/sha}',
+    git_tags_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/git/tags{/sha}',
+    git_refs_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/git/refs{/sha}',
+    trees_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/git/trees{/sha}',
+    statuses_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/statuses/{sha}',
+    languages_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/languages',
+    stargazers_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/stargazers',
+    contributors_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/contributors',
+    subscribers_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/subscribers',
+    subscription_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/subscription',
+    commits_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/commits{/sha}',
+    git_commits_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/git/commits{/sha}',
+    comments_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/comments{/number}',
+    issue_comment_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/issues/comments{/number}',
+    contents_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/contents/{+path}',
+    compare_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/compare/{base}...{head}',
+    merges_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/merges',
+    archive_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/{archive_format}{/ref}',
+    downloads_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/downloads',
+    issues_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/issues{/number}',
+    pulls_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/pulls{/number}',
+    milestones_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/milestones{/number}',
+    notifications_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/notifications{?since,all,participating}',
+    labels_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/labels{/name}',
+    releases_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/releases{/id}',
+    deployments_url:
+      'https://api.github.com/repos/Bel-Albuquerque/awesome-compose/deployments',
+    created_at: '2022-05-06T17:27:36Z',
+    updated_at: '2022-05-26T17:23:45Z',
+    pushed_at: '2022-04-28T14:49:18Z',
+    git_url: 'git://github.com/Bel-Albuquerque/awesome-compose.git',
+    ssh_url: 'git@github.com:Bel-Albuquerque/awesome-compose.git',
+    clone_url: 'https://github.com/Bel-Albuquerque/awesome-compose.git',
+    svn_url: 'https://github.com/Bel-Albuquerque/awesome-compose',
+    homepage: 'https://docs.docker.com/compose/',
+    size: 5052,
+    stargazers_count: 2,
+    watchers_count: 2,
+    language: null,
+    has_issues: false,
+    has_projects: true,
+    has_downloads: true,
+    has_wiki: true,
+    has_pages: false,
+    forks_count: 3,
+    mirror_url: null,
+    archived: false,
+    disabled: false,
+    open_issues_count: 0,
+    license: {
+      key: 'cc0-1.0',
+      name: 'Creative Commons Zero v1.0 Universal',
+      spdx_id: 'CC0-1.0',
+      url: 'https://api.github.com/licenses/cc0-1.0',
+      node_id: 'MDc6TGljZW5zZTY=',
+    },
+    allow_forking: true,
+    is_template: false,
+    web_commit_signoff_required: false,
+    topics: [],
+    visibility: 'public',
+    forks: 3,
+    open_issues: 0,
+    watchers: 2,
+    default_branch: 'master',
+  },
+];
