906a6cddf2abe8f397eee1c7ca5f315b8e535123
leandrochs <leandrochagask@gmail.com>
2022-08-08 10:29:42 -0300

    emproe: remove comentários

--- src/services/getApiByName.js
+++
@@ -1,1 +0,0 @@
-// https://api.github.com/search/users?q=%22leandro%20Silva%22
--- src/services/getApiByUserName.js
+++ src/services/getApiByUserName.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 function errorHandling(response) {
-  console.log('response: ', response);
   if (response?.login && response?.id && response?.repos_url) {
     return response;
   } else if (
@@ -37,36 +36,3 @@ async function getApiByUserName(userName) {
 export default getApiByUserName;
 
 // seguir usuário: https://docs.github.com/pt/rest/users/followers
-
-// erro de excesso de requisição: {
-//     "message": "Request failed with status code 403",
-//     "name": "AxiosError",
-//     "config": {
-//         "transitional": {
-//             "silentJSONParsing": true,
-//             "forcedJSONParsing": true,
-//             "clarifyTimeoutError": false
-//         },
-//         "transformRequest": [
-//             null
-//         ],
-//         "transformResponse": [
-//             null
-//         ],
-//         "timeout": 0,
-//         "xsrfCookieName": "XSRF-TOKEN",
-//         "xsrfHeaderName": "X-XSRF-TOKEN",
-//         "maxContentLength": -1,
-//         "maxBodyLength": -1,
-//         "env": {
-//             "FormData": null
-//         },
-//         "headers": {
-//             "Accept": "application/json, text/plain, */*"
-//         },
-//         "method": "get",
-//         "url": "https://api.github.com/users/leandrochs"
-//     },
-//     "code": "ERR_BAD_REQUEST",
-//     "status": 403
-// }
