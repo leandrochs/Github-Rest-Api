e0af5c8ce903abd0b955f8c7a24dc9d668771e88
leandrochs <leandrochagask@gmail.com>
2022-08-07 22:45:21 -0300

    feat: adiciona resumo de repositorio

--- src/components/repositoriesColumn/RepositoriesColumn.jsx
+++ src/components/repositoriesColumn/RepositoriesColumn.jsx
@@ -1,22 +1,69 @@
 import React, { useContext, useEffect } from 'react';
 import Context from '../../context/Context';
+import getApiRepositories from '../../services/getApiRepositories';
 import { githubUser as userProfile } from '../../tests/Mocks/githubUser';
+import { repositoriesResponse as repositories } from '../../tests/Mocks/repositoriesResponse';
+import ForkdIcon from '../svg/forkdIcon/ForkdIcon';
 import './repositoriesColumn.css';
 
 export default function RepositoriesColumn() {
   // const { userProfile, setUserProfile } = useContext(Context);
   const { setUserProfile } = useContext(Context);
+  // const { repositories, setRepositories } = useContext(Context);
+  const { setRepositories } = useContext(Context);
+
+  // async function getApi() {
+  //   const response = await getApiRepositories(userProfile.login);
+
+  //   setRepositories(response);
+  // }
 
   // useEffect(() => {
-  //   setUserProfile(userProfile);
-  // }, [setUserProfile]);
-
-  // console.log(userProfile);
+  //   getApi();
+  // }, []);
 
   return (
+    <div className='repositoriesColumn-container'>
+      {repositories &&
+        repositories.map(
+          ({ name, html_url, description, language, forks, license }) => (
+            <div key={name} className='repository-summary-container'>
+              <div className='repositoriesColumn-github-Link-container'>
+                <a
+                  className='repositoriesColumn-github-Link'
+                  href={html_url}
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  {name}
+                </a>
+              </div>
+              <div className='repositoriesColumn-description'>
+                {description}
+              </div>
+              <div className='repositoriesColumn-language-fork-licence'>
+                <span className='repositoriesColumn-language-color'></span>
 
-        <div className='repositoriesColumn-container'>
-          repositories
-        </div>
+                {language && (
+                  <div className='repositoriesColumn-language'>{language}</div>
+                )}
+
+                {forks === 0 ? (
+                  <div className='repositoriesColumn-fork'>
+                    <ForkdIcon />
+                    {forks}
+                  </div>
+                ) : null}
+
+                {license && (
+                  <div className='repositoriesColumn-licence'>
+                    {license?.name}
+                  </div>
+                )}
+              </div>
+            </div>
+          ),
+        )}
+    </div>
   );
 }
