3337395f0b1cefa9f62fb1d303577fccc8c5152f
leandrochs <leandrochagask@gmail.com>
2022-08-08 10:29:59 -0300

    emproe: remove comentários

--- src/pages/searchPage/SearchPage.jsx
+++ src/pages/searchPage/SearchPage.jsx
@@ -1,18 +1,13 @@
 import React, { useContext } from 'react';
-// import { Redirect } from 'react-router-dom';
 import Form from '../../components/form/Form';
 import GithubLogo from '../../components/githubLogo/GithubLogo';
-// import Context from '../context/Context';
 import './searchPage.css';
 
 export default function SearchPage() {
-  // const { userProfile } = useContext(Context);
-
   return (
     <main className='searchPage-main'>
       <GithubLogo />
       <Form />
-      {/* {userProfile.length > 0 ? <Redirect to='/pageresult' /> : <React.Fragment /> } */}
     </main>
   );
 }
